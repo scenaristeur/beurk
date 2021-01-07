038ca4335e7f44a4586ba860a0be718f9b5999ad
Scenaristeur <scenaristeur@gmail.com>
2021-01-07 21:36:58 +0100

    review

--- src/vis.js
+++ src/vis.js
@@ -21,20 +21,24 @@ function vis(data) {
         n = node_exist[0]
         //  console.log("exist",n)
       }else{
-        n  = {id: uuidv4()}
-        if(node.length > 30){
-          n.label = node.substring(0,30)+'...'
-          n.title = node
+        if (typeof node =="string"){
+          n  = {id: uuidv4()}
+          if(node.length > 30){
+            n.label = node.substring(0,30)+'...'
+            n.title = node
+          }else{
+            n.label = node
+          }
+          dataset.nodes.push(n)
         }else{
-          n.label = node
+          console.log(typeof node, node)
         }
-        dataset.nodes.push(n)
       }
       let edge = {from:o.id, to: n.id, arrows: "to", label: prop}
       //  console.log(typeof node , edge)
       dataset.edges.push(edge)
     }
-    
+
   });
   console.log("DS",dataset)
   return dataset
